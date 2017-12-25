a30a3d3bd32ddcd68659199000d2e11e0003d50a
Tommy Holladay <thomaswholladay@gmail.com>
2017-12-25 17:35:33 -0500

    Piano without Ng-audio

--- src/app/common/services/keyboard.factory.js
+++ src/app/common/services/keyboard.factory.js
@@ -1,7 +1,7 @@
 (function(){
 	angular.module('MyApp.Common')
 
-	.factory("Keyboard", function(frequencyList, AudioContext, Volume, ngAudio) {
+	.factory("Keyboard", function(frequencyList, AudioContext, Volume, ngAudio, BufferLoader) {
 		function Keyboard() {
 			this.myAudioContext  = AudioContext.get();
 	  		this.frequency = 440;
@@ -10,11 +10,11 @@
 	  		this.gainNode = null;
 	  		this.file = null;
 	  		this.isPlaying = false;
-	  		this.pianoNotes = new Array(25);
-	  		this.note = -1
-	  		for(var i = 0; i < 25; i++){
-	  			this.pianoNotes[i] = ngAudio.load("app/common/sounds/Piano/" + (i + 28) + ".mp3");
-	  		}
+	  		// this.pianoNotes = new Array(25);
+	  		// this.note = -1
+	  		// for(var i = 0; i < 25; i++){
+	  		// 	this.pianoNotes[i] = ngAudio.load("app/common/sounds/Piano/" + (i + 28) + ".mp3");
+	  		// }
 		}
 
 		Keyboard.prototype.setWaveType = function(wave) {
@@ -65,29 +65,59 @@
 			// if (isPlaying) {
 			// 	this.file.stop();
 			// }
-			if(this.isPlaying = true) {
-				this.file.stop();
-				this.isPlaying= false;
-				this.pianoNotes[this.note] = ngAudio.load("app/common/sounds/Piano/" + (this.note + 28) + ".mp3");
-			}
+			// if(this.isPlaying = true) {
+			// 	this.file.stop();
+			// 	this.isPlaying= false;
+			// 	this.pianoNotes[this.note] = ngAudio.load("app/common/sounds/Piano/" + (this.note + 28) + ".mp3");
+			// }
+			// this.source.stop(this.context.currentTime)
 		}
 
 		Keyboard.prototype.playPianoNote = function(note) {
-      		var isPlaying = this.file && this.file.currentTime > 0 && !this.file.paused && !this.file.ended && this.file.readyState > 2;
-			if(!isPlaying) {
-				// note = parseInt(note) + 28;
-				//this.file = ngAudio.load("app/common/sounds/Piano/" + note + ".mp3");
-				note = parseInt(note);
-				this.file = this.pianoNotes[note];
-				this.file.volume = Volume.get();
-				this.file.play();
-				this.isPlaying = true;
-				this.note = note
-			}
-			else {
-				this.stopPianoNote();
+			var note = parseInt(note ) + 28;
+
+			var bufferLoader = new BufferLoader(
+				this.myAudioContext,
+			    ["app/common/sounds/Piano/" + note + ".mp3"],
+			    finishedLoading
+		    );
+
+		  	bufferLoader.load();
+
+		  	function finishedLoading(bufferList) {
+				// Create two sources and play them both together.
+				source = this.context.createBufferSource();
+				source.buffer = bufferList[0];
+				var gainNode = this.context.createGain();
+
+				gainNode.gain.value = Volume.get();
+
+				source.connect(gainNode);
+
+				gainNode.connect(this.context.destination);
+
+				source.start(this.context.currentTime);
+				source.stop(this.context.currentTime+.5);
 			}
 		}
+
+		// Keyboard.prototype.playPianoNote = function(note) {
+    		// var isPlaying = this.file && this.file.currentTime > 0 && !this.file.paused && !this.file.ended && this.file.readyState > 2;
+			// if(!isPlaying) {
+			// 	// note = parseInt(note) + 28;
+			// 	//this.file = ngAudio.load("app/common/sounds/Piano/" + note + ".mp3");
+			// 	note = parseInt(note);
+			// 	this.file = this.pianoNotes[note];
+			// 	this.file.volume = Volume.get();
+			// 	this.file.play();
+			// 	this.isPlaying = true;
+			// 	this.note = note
+			// }
+			// else {
+			// 	this.stopPianoNote();
+			// }
+
+		// }
 
 		return Keyboard;
 	});
